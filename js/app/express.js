bed4f19e79337daa13ed6caccdc510845758a688
qudenger <qudenger@icloud.com>
2015-09-22 15:49:07 +0800

    add copyright info

--- js/app/express.js
+++ js/app/express.js
@@ -1,3 +1,13 @@
+/**
+ *
+ * Copyright (c) 2006 - 2015 xinao
+ *
+ * @category   
+ * @package   
+ * @copyright 
+ * @license    
+ * @version   
+ */
 var express = require('express')
 var app = express()
 
