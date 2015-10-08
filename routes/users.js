6a377557e04cb9a68154598819f32182afb91223
dxcqcv <dxcqcv@gmail.com>
2015-10-08 15:47:09 +0800

    update gyt

--- routes/users.js
+++ routes/users.js
@@ -16,11 +16,8 @@ router.get('/home', function(req, res) {
 router.get('/nxjc', function(req, res) {
     res.render('nxjc', { title: 'Home' });
 });
-router.get('/gyt', function(req, res) {
+router.get('/gyjc', function(req, res) {
     res.render('gyt', { title: 'Home' });
-});
-router.get('/hc', function(req, res) {
-    res.render('hightcharts', { title: 'Home' });
 });
 router.get('/xmgl', function(req, res) {
     res.render('xmgl', { title: 'Home' });
