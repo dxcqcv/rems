03e52935888678b3e40c164ef9fca9f2533ea870
Roy <dxcqcv@gmail.com>
2015-10-12 17:31:10 +0800

    update

--- public/js/app/nxjc.js
+++ public/js/app/nxjc.js
@@ -1,10 +1,134 @@
 
 define(function(require){
-    var $ = require('jquery')
+    var 
+        nxjcCss = require('css!../../css/nxjc')
+      , selectpickerCss = require('css!../../css/bootstrap-select')
+      , $ = require('jquery')
       , highcharts = require('highcharts')
-      , nxjcCss = require('css!../../css/nxjc')
+      , selectpicker = require('bootstrap-select')
+      , datapicker = require('bootstrap-datetimepicker.min')
       ;
       (function(){
+      //下拉选择
+      $('.selectpicker').selectpicker({
+      });
+      //图表
+      //fourth
+        $('#container').highcharts({
+            chart: {
+                type: 'pie' //图表类型还是pie图
+            },	    
+            plotOptions: {
+                pie: {
+                    innerSize: '20' //也可以配置为10%的百分比形式
+                }
+            },
+            credits:{
+                text:"www.stepday.com",
+                href:"http://www.stepday.com"
+            },
+            series: [{
+                data: [
+                    ['Firefox',   44.2],
+                    ['IE7',       26.6],
+                    ['IE6',       20],
+                    ['Chrome',    3.1],
+                    ['Other',    5.4]
+                ]
+            }]
+        });
+      //third
+          $('#gongnenghaonengCharts').highcharts({
+        chart: {
+            type: 'column'
+        },
+
+        title: {
+            text: null,
+        },
+        subtitle: {
+            text: null,
+        },
+        xAxis: {
+            categories: [
+                'Jan',
+                'Feb',
+                'Mar',
+                'Apr',
+                'May',
+                'Jun',
+                'Jul',
+                'Aug',
+                'Sep',
+                'Oct',
+                'Nov',
+                'Dec'
+            ],
+            crosshair: true
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Rainfall (mm)'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'Tokyo',
+            data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+
+        }, {
+            name: 'New York',
+            data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+
+        }]
+    });
+      //second
+      $('#gyxtxl').highcharts({
+        title: {
+            text: null,
+        },
+        subtitle: {
+            text: null,
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'Temperature (°C)'
+            },
+            plotLines: [{
+                value: 20,
+                width: 1,
+                //color: '#808080'
+                zIndex: 2,
+                color: 'red'
+            }]
+        },
+        tooltip: {
+            valueSuffix: '°C'
+        },
+        series: [{
+            name: 'Tokyo',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        }]
+    });
+      //first
             $('.chart-box').highcharts({
                 chart: {
                     type: 'area'
