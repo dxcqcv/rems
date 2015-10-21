5b64d4018be0b87f197f3c635cf1ff6b5f4c31e8
qudenger <qudenger@qq.com>
2015-10-21 16:08:16 +0800

    图表的一些点击事件功能

--- public/js/app/nxfx.js
+++ public/js/app/nxfx.js
@@ -5,6 +5,8 @@ define(function(require){
       , datapicker = require('bootstrap-datetimepicker.min')
       , exporting = require('exporting')
       ;
+
+
       (function(){
       //日期控件
        $('.datetimepicker1').datetimepicker();
@@ -12,43 +14,138 @@ define(function(require){
       //下拉选择
       $('.selectpicker').selectpicker({
       });
+
+
       //图表
 
-    $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {
+    // $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {
+    //     // Create the chart
+    //     var nxfxChart = $('.chart-box').highcharts('StockChart', {
+
+
+    //         rangeSelector : {
+    //             selected : 1
+    //         },
+
+    //         title : {
+    //             text : null 
+    //         },
+
+    //     yAxis: {
+    //         title: {
+    //             //text: 'Temperature (°C)'
+    //             text: null 
+    //         },
+    //         plotLines: [{
+    //             value: 125,
+    //             width: 1,
+    //             //color: '#808080'
+    //             zIndex: 2,
+    //             color: 'red'
+    //         }]
+    //     },
+    //     rangeSelector: {
+    //             selected: 1,
+    //             inputDateFormat: '%Y-%m-%d'
+    //         },
+    //         series : [{
+    //             name : 'AAPL',
+    //             data : data,
+    //             tooltip: {
+    //                 valueDecimals: 2
+    //             }
+    //         }]
+    //     });
+
+    //     setTimeout(function() {
+    //         $('input.highcharts-range-selector', $('#' + nxfxChart.options.chart.renderTo)).datetimepicker()
+    //     }, 0)
+
+    // });
+
+
+$.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function(data) {
         // Create the chart
-        $('.chart-box').highcharts('StockChart', {
-
-
-            rangeSelector : {
-                selected : 1
+        window.chart = new Highcharts.StockChart({
+            chart: {
+                renderTo: 'qxxxChartBox'
             },
 
-            title : {
-                text : null 
+            rangeSelector: {
+                selected: 1,
+                inputDateFormat: '%Y-%m-%d',
+
+                buttons: [{
+                    type: 'day',
+                    count: 1,
+                    text: '日'
+                }, {
+                    type: 'month',
+                    count: 1,
+                    text: '月'
+                }, {
+                    type: 'year',
+                    count: 1,
+                    text: '年'
+                }],
+                buttonTheme: { // styles for the buttons
+                    fill: 'none',
+                    stroke: 'none',
+                    'stroke-width': 10,
+                    r: 8,
+                    style: {
+                        color: '#039',
+                        fontWeight: 'bold',
+                        
+                    },
+                    states: {
+                        hover: {
+                        },
+                        select: {
+                            fill: '#039',
+                            style: {
+                                color: 'red'
+                            }
+                        }
+                    }
+                },
             },
 
-        yAxis: {
             title: {
-                //text: 'Temperature (°C)'
-                text: null 
+                text: ''
             },
-            plotLines: [{
-                value: 125,
-                width: 1,
-                //color: '#808080'
-                zIndex: 2,
-                color: 'red'
-            }]
-        },
-            series : [{
-                name : 'AAPL',
-                data : data,
+
+            series: [{
+                name: 'AAPL',
+                data: data,
                 tooltip: {
                     valueDecimals: 2
-                }
-            }]
+                }}]
+
+        }, function(chart) {
+
+            // apply the date pickers
+            setTimeout(function() {
+                $('input.highcharts-range-selector', $('#' + chart.options.chart.renderTo)).datetimepicker({
+    format: 'yyyy-mm-dd hh:ii'
+})
+            }, 0);
+             // var chooseContainer = $('<div class="pull-left cbfx-date-box"></div>');
+             // var chooseRange = $('<select class="form-control"></select>');
+             // var element1 = $('<option value="volvo">请选择工艺系统</option>');
+             // var element2 = $('<option value="saab">Saab</option>');
+             // chooseContainer.append(chooseRange);
+             // chooseRange.append(element1);
+             // chooseRange.append(element2);
+             // //$("#qxxxChartBox .highcharts-range-selector-buttons").append(chooseContainer);
+             // $("#qxxxChartBox .highcharts-container").before(chooseContainer);
+             // //console.log($("#qxxxChartBox").find(".highcharts-range-selector-buttons").eq(0));
+
+
         });
     });
+
+
 
     $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {
         // Create the chart
--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/restlogin.json', function(req, res, next) {
 
-	request.post({url:'http://10.20.1.169:8080/rems/innerlogin.json', form: {username:'enn_admin',password:'123456'}}, function(error,response,body){
+	request.post({url:'http://10.20.1.3:8080/rems/login.json', form: {username:'enn_admin',password:'123456',sign:"b039e34c65cdde6288ef86d340784a4f"}}, function(error,response,body){
 		
 	    	res.send(body);
 	  	
@@ -14,7 +14,7 @@ router.get('/restlogin.json', function(req, res, next) {
 
 router.get('/gislist.json', function(req, res, next) {
 
-	request.post({url:'http://10.20.1.169:8080/rems/gislist.json', form: {username:'enn_admin',password:'123456'}}, function(error,response,body){
+	request.post({url:'http://10.20.1.3:8080/rems/userInfo/list.json', form: {username:'enn_admin',password:'123456'}}, function(error,response,body){
 		
 	    	res.send(response);
 	  	
