276a69474b27e91955bac646353dbde6f48ed71c
qudenger <qudenger@qq.com>
2015-10-28 10:45:06 +0800

    增加供能分析的弹出框完美显示图表

--- public/js/app/gnfx.js
+++ public/js/app/gnfx.js
@@ -9,15 +9,74 @@ define(function(require) {
     //弹出层
     $('.gnhnIcon').on('click', function() {
         $("#myModal").modal('show');
-         localJsonp.start({url:jsonpPath+'modalLines.js',jsonpCallback:'modalLines',done:modalLines});
+        // localJsonp.start({url:jsonpPath+'modalLines.js',jsonpCallback:'modalLines',done:modalLines});
     });
+
+    
+    $('#myModal').on('shown.bs.modal', function() {
+        localJsonp.start({url:jsonpPath+'modalLines.js',jsonpCallback:'modalLines',done:modalLines});
+        $('#gnhnHaodianCharts').css('visibility', 'hidden');
+        $('#gnhnHaodianCharts').css('visibility', 'initial');  
+    }); 
+  
+
+
     function modalLines(data) {
-    console.log(data)
-        options.chart.renderTo = 'gnhnHaodianCharts';
-        options.chart.type= 'line';
-        options.xAxis.categories = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-        options.series[0].data = data;
-        chart = new Highcharts.Chart(options); 
+        //console.log(data)
+        // options.chart.renderTo = 'gnhnHaodianCharts';
+        // options.chart.type= 'line';
+        // options.xAxis.categories = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        // options.series[0].data = data;
+        // chart = new Highcharts.Chart(options); 
+
+    $('#gnhnHaodianCharts').highcharts({
+            chart: {
+                type: 'column'        
+            },
+            title: {
+                text: 'Stacked column chart'
+            },
+            xAxis: {
+                categories: ['Lazada', 'Competitor 1', 'Competitor 2', 'Competitor 3', 'Competitor 4']
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Price Range'
+                }
+            },
+            tooltip: {
+                pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage:.0f}%)<br/>',
+                shared: true
+            },
+            plotOptions: {
+                column: {
+                    stacking: 'percent'
+                }
+            },
+            series: [{
+                name: '100 - 300',
+                data: [5, 3, 4, 7, 2]
+            }, {
+                name: '301 - 500',
+                data: [2, 2, 3, 2, 1]
+            }, {
+                name: '501 - 1000',
+                data: [3, 4, 4, 2, 5]
+            }, {
+                name: '1001 - 3000',
+                data: [3, 4, 4, 2, 5]
+            }, {
+                name: '3001 - 5000',
+                data: [3, 4, 4, 2, 5]
+            }, {
+                name: '5001 - 10000',
+                data: [3, 4, 4, 2, 5]
+            }]
+        });
+        var chart = $('#gnhnHaodianCharts').highcharts();
+
+        
     }
     // tooltips
          $('[data-toggle="tooltip"]').tooltip();  
@@ -72,5 +131,10 @@ define(function(require) {
             options.series[0].data = data;
             chart = new Highcharts.Chart(options); 
          }
+
+
+
+
+
     }());
 });
