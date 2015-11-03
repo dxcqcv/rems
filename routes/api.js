4aa8c33fa7d8b9cdb5c1091979b022fe915e61c1
qudenger <qudenger@qq.com>
2015-11-03 16:58:42 +0800

    更新API接口文件

--- routes/api.js
+++ routes/api.js
@@ -24,7 +24,7 @@ router.post('/login.json', function(req, res, next) {
     var str = buf.toString("binary");
     var crypto = require("crypto");
     var d = crypto.createHash("md5").update(str).digest("hex").toUpperCase();
-	request.post({url:'http://117.144.16.98:8080/rems/login.json', form: {username:req.body.username,password:req.body.password,sign:d}}, function(error,response,body){
+	request.post({url:'http://10.20.1.3:8080/rems/login.json', form: {username:req.body.username,password:req.body.password,sign:d}}, function(error,response,body){
 	    	//res.send(body);
 	    	var result = JSON.parse(body); 
 	    	if (result.status.code == 200) {
@@ -49,13 +49,42 @@ router.post('/login.json', function(req, res, next) {
 router.get('/gislist.json', function(req, res, next) {
 
 	//request.post({url:'http://10.20.1.3:8080/rems/userInfo/list.json', form: {userKey:'cf76a0b9e6bae5b0a4e416754588328d',password:'123456'}}, function(error,response,body){
-	request.post({url:'http://117.144.16.98:8080/rems/gislist.json', form: {userKey:req.session.user.token}}, function(error,response,body){
+	request.post({url:'http://10.20.1.3:8080/rems/gislist.json', form: {userKey:req.session.user.token}}, function(error,response,body){
 		
 	    	res.send(body);
 	  	
 	})
 
 });
+
+router.get('/clickProject.json', function(req, res, next) {
+	request.post({url:'http://10.20.1.3:8080/rems/clickProject.json', form: {userKey:req.session.user.token, projectid:req.body.projectid}}, function(error,response,body){
+	    	res.send(body);	
+	})
+});
+
+router.get('/projectOverview.json', function(req, res, next) {
+	request.post({url:'http://10.20.1.3:8080/rems/projectOverview.json', form: {userKey:req.session.user.token, projectid:req.body.projectid, overviewclass:req.body.overviewclass}}, function(error,response,body){
+	    	res.send(body);	
+	})
+});
+
+router.get('/techCheck/equipments.json', function(req, res, next) {
+	request.post({url:'http://10.20.1.3:8080/rems/techCheck/equipments.json', form: {userKey:req.session.user.token, projectid:req.body.projectid, pageid:req.body.pageid}}, function(error,response,body){
+	    	res.send(body);	
+	})
+});
+router.get('/techCheck/equState.json', function(req, res, next) {
+	request.post({url:'http://10.20.1.3:8080/rems/techCheck/equState.json', form: {userKey:req.session.user.token, projectid:req.body.projectid, pageid:req.body.pageid}}, function(error,response,body){
+	    	res.send(body);	
+	})
+});
+
+// router.get('/gislist.json', function(req, res, next) {
+// 	request.post({url:'http://117.144.16.98:8080/rems/gislist.json', form: {userKey:req.session.user.token}}, function(error,response,body){
+// 	    	res.send(body);	
+// 	})
+// });
 
 
 module.exports = router;
