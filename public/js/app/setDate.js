af48ac265ca7adeb7bebce2d48952108e49ecbad
Roy <dxcqcv@gmail.com>
2015-11-30 09:47:50 +0800

    update setDate

--- public/js/app/setDate.js
+++ public/js/app/setDate.js
@@ -25,26 +25,31 @@ define(function(require){
             var el = $(el);
             var fromEl = el.attr('data-tar');
             var fromTime, toTime;
+            var d = new Date();
             if(fromEl === undefined) return;
             if (el.attr('data-range') == '1d') {
                 //fromTime = moment().startOf('day').format("YYYY-MM-DD");
                 fromTime = moment().format("YYYY-MM-DD");
                 //console.log(fromTime)
                 //$("#" + fromEl).data("DateTimePicker").date(fromTime).format("YYYY-MM-DD").viewMode('days');
-$("#" + fromEl).data("DateTimePicker").destroy();
- $("#" + fromEl).datetimepicker(datetimepickerObj4)
+                $("#" + fromEl).data("DateTimePicker").date(d).format("YYYY-MM-DD").viewMode('days');
+//$("#" + fromEl).data("DateTimePicker").destroy();
+ //$("#" + fromEl).datetimepicker(datetimepickerObj4)
             }else if (el.attr('data-range') == '1m') {
                 fromTime = moment().format("YYYY-MM");
                 //$("#" + fromEl).data("DateTimePicker").date(fromTime).format("YYYY-MM").viewMode('months');
-$("#" + fromEl).data("DateTimePicker").destroy();
- $("#" + fromEl).datetimepicker(datetimepickerObj3)
+                $("#" + fromEl).data("DateTimePicker").date(d).format("YYYY-MM").viewMode('months');
+//$("#" + fromEl).data("DateTimePicker").destroy();
+ //$("#" + fromEl).datetimepicker(datetimepickerObj3)
             }else{
                 fromTime = moment().format("YYYY");
+                //console.log(fromTime);
                 //$("#" + fromEl).data("DateTimePicker").date(fromTime).format("YYYY").viewMode('years');
+                $("#" + fromEl).data("DateTimePicker").date(d).format("YYYY").viewMode('years');
                 //console.log('fromEl',fromEl)
 //$("#" + fromEl).data("DateTimePicker").destroy().datetimepicker(datetimepickerObj2);
-$("#" + fromEl).data("DateTimePicker").destroy();
- $("#" + fromEl).datetimepicker(datetimepickerObj2)
+//$("#" + fromEl).data("DateTimePicker").destroy();
+ //$("#" + fromEl).datetimepicker(datetimepickerObj2)
 //$("#" + fromEl).data("DateTimePicker").options(datetimepickerObj2);
             };
             var datetime = $("#" + fromEl).find('input').val();
