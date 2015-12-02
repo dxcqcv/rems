77044536a9908e904661c5fa19570dd89af6bc18
zhengjf1988 <zhengjf1988@163.com>
2015-12-02 17:35:57 +0800

    耗能分析

--- public/js/app/hnfx.js
+++ public/js/app/hnfx.js
@@ -7,7 +7,7 @@ define(function(require) {
 		highcharts = require('exporting'),
 		globalTools = require('app/globalTools'),
 		options = require('app/highchartsConfig'),
-        projectid = require('app/checkProjectid'),
+		projectid = require('app/checkProjectid'),
 		api = require('app/getApi'),
 		optionsLines = require('app/highchartsConfigLines'),
 		datetimepickerObj = require('app/dateObj');
@@ -117,9 +117,15 @@ define(function(require) {
 		demand.start({
 			url: '/api/consumptionEnergyInfo/list.json',
 			parameter: {
-                dir: {today:'drgnsp',yestday:'qrgnsp',month:'dygnsp',year:'dngnsp'},
-                id: 'drgnsp',
-				options: options
+				dir: {
+					today: 'drgnsp',
+					yestday: 'qrgnsp',
+					month: 'dygnsp',
+					year: 'dngnsp'
+				},
+				id: 'drgnsp',
+				options: options,
+				moduleFlag: 1 //标示是详细页面那个模块的id(1当日详细2当月详细3昨日详细4当年详细)
 			},
 			data: {
 				projectid: projectid,
@@ -130,7 +136,7 @@ define(function(require) {
 
 
 		function res(data, parameter) {
-        console.log(data)
+			//			console.log(data)
 			var result = data.status.data;
 
 			var xData = new Array;
@@ -138,26 +144,32 @@ define(function(require) {
 
 			//---------------总的页面----------------------
 			//-------------------当日--------------------
-            $.each(result,function(i,v){
-            //console.log(i)
-                $.each(parameter.dir, function(k,p){
-                    if(i === k){
-                        switch(i) {
-                            case 'month': builtGhPage(v,p,parameter,2); break;
-                            case 'year': builtGhPage(v,p,parameter,3); break;
-                            default: builtGhPage(v,p,parameter,1);break;
-                        }
-                    }   
-                })
-            });
+			$.each(result, function(i, v) {
+				//console.log(i)
+				$.each(parameter.dir, function(k, p) {
+					if (i === k) {
+						switch (i) {
+							case 'month':
+								builtGhPage(v, p, parameter, 2);
+								break;
+							case 'year':
+								builtGhPage(v, p, parameter, 3);
+								break;
+							default:
+								builtGhPage(v, p, parameter, 1);
+								break;
+						}
+					}
+				})
+			});
 
 			//var today = data.status.data.today;
 			//var dataItem = [];
 			//$.each(today.totalData, function(i, v) {
-				//dataItem = new Array;
-				//dataItem.push(globalTools.dateFormterItem(1, i));
-				//dataItem.push(v);
-				//xData.push(dataItem);
+			//dataItem = new Array;
+			//dataItem.push(globalTools.dateFormterItem(1, i));
+			//dataItem.push(v);
+			//xData.push(dataItem);
 			//});
 			//today.data = xData;
 
@@ -182,10 +194,10 @@ define(function(require) {
 			//var yestday = data.status.data.yestday;
 			//dataItem = new Array;
 			//$.each(yestday.totalData, function(i, v) {
-				//dataItem = new Array;
-				//dataItem.push(globalTools.dateFormterItem(1, i));
-				//dataItem.push(v);
-				//xData.push(dataItem);
+			//dataItem = new Array;
+			//dataItem.push(globalTools.dateFormterItem(1, i));
+			//dataItem.push(v);
+			//xData.push(dataItem);
 			//});
 			//yestday.data = xData;
 
@@ -211,10 +223,10 @@ define(function(require) {
 			//sData = new Array;
 			//dataItem = new Array;
 			//$.each(month.totalData, function(i, v) {
-				//dataItem = new Array;
-				//dataItem.push(globalTools.dateFormterItem(2, i));
-				//dataItem.push(v);
-				//xData.push(dataItem);
+			//dataItem = new Array;
+			//dataItem.push(globalTools.dateFormterItem(2, i));
+			//dataItem.push(v);
+			//xData.push(dataItem);
 			//});
 			//month.data = xData;
 
@@ -241,10 +253,10 @@ define(function(require) {
 			//sData = new Array;
 			//dataItem = new Array;
 			//$.each(year.totalData, function(i, v) {
-				//dataItem = new Array;
-				//dataItem.push(globalTools.dateFormterItem(3, i));
-				//dataItem.push(v);
-				//xData.push(dataItem);
+			//dataItem = new Array;
+			//dataItem.push(globalTools.dateFormterItem(3, i));
+			//dataItem.push(v);
+			//xData.push(dataItem);
 			//});
 			//year.data = xData;
 
@@ -269,36 +281,130 @@ define(function(require) {
 
 
 			//---------------详细页面----------------------
-			//-------------------当日--------------------
-			xData = new Array;
-			sData = new Array;
+			//-------------------当日详细(需要把今日数据与昨日数据都取出来)--------------------
 			var today = data.status.data.today.resList;
-			//console.log(today);
-			$.each(today, function(i, v) {
-				xData.push(globalTools.dateFormterItem(1, v.rectime), v);
-				sData.push(v);
+			var yestday = data.status.data.yestday.resList;
+			
+			var todayData = dataFormater(today);
+
+			var yestdayData = dataFormater(yestday);
+
+			//			console.log(todayData);
+			console.log(yestdayData);
+
+			var resMap = [];
+			$.each(todayData, function(i, v) {
+				var list2 = new Array;
+				$.each(yestdayData, function(y, m) {
+					if (m.name == v.name) {
+						list2 = v.list;
+						return false;
+					}
+				});
+				var resList = returnResult(v.list, list2, 1, 1);
+
+				var item = new Object;
+				item.name = v.name;
+				item.list = resList;
+				resMap.push(item);
+
+				console.log(resMap);
 			});
-			today.xData = xData;
-			//			today.sData = sData;
-			today.line; // 约束性指标
-			today.line1; // 引导性指标
-			var baseLines = [];
-			var line = {};
-			line.vaule = today.line;
-			baseLines.push(line);
-			var line = {};
-			line.vaule = today.line1;
-			baseLines.push(line);
+		}
 
-			today.baseLines = baseLines;
+		function returnResult(realdata, olddata, dateFlag, moduleFlag) {
+			var sData = new Array();
+			var xData = new Array();
+			var sDataElement = new Object();
 
-			//-------------------当日--------------------
-			//globalTools.uniq();();
+			sDataElement.data = new Array();
+
+			var sDataElementOld = new Object();
+
+			sDataElementOld.data = new Array();
+			var temp = new Object();
+
+			if (moduleFlag == 1) {
+				sDataElement.name = "今天";
+				sDataElementOld.name = "昨天";
+			}
+			if (moduleFlag == 2) {
+				sDataElement.name = "当月";
+				sDataElementOld.name = "上月";
+			}
+			if (moduleFlag == 3) {
+				sDataElement.name = "昨天";
+				sDataElementOld.name = "前天";
+			}
+			if (moduleFlag == 4) {
+				sDataElement.name = "当年";
+				sDataElementOld.name = "去年";
+			}
+
+			//今年数据解析
+			for (var i = 0; i < realdata.length; i++) {
+				temp = new Object();
+				temp.name = globalTools.dateFormterItem(dateFlag, realdata[i].rectime);
+				xData.push(temp.name);
+
+				temp.y = parseFloat(realdata[i].datavalue);
+				sDataElement.data.push(temp);
+			}
+
+			//去年数据解析
+			for (var j = 0; j < olddata.length; j++) {
+				temp = new Object();
+				temp.name = globalTools.dateFormterItem(dateFlag, olddata[j].rectime);
+				xData.push(temp.name);
+				temp.y = parseFloat(olddata[j].datavalue);
+				sDataElementOld.data.push(temp);
+			}
+
+			sData.push(sDataElement);
+			sData.push(sDataElementOld);
+
+			xData = globalTools.uniq(xData);
+
+			var tmp = new Array();
+			var item = {};
+
+			item.xData = xData;
+			item.sData = sData;
+
+			tmp.push(item);
+			return tmp;
+		}
 
 
 
+		//对LIst集合数据进行筛选取值，最终返回一个数组
+		function dataFormater(data) {
+			var resultMap = [];
+			$.each(data, function(i, v) {
+				var res = containsKey(resultMap, v.showname);
+				if (res == undefined) {
+					var list = new Array;
+					list.push(v);
+					var item = {};
+					item.name = v.showname;
+					item.list = list;
+					resultMap.push(item)
+				} else {
+					res.push(v);
+				}
+			});
+			return resultMap;
+		}
 
-			//globalTools.ghnCallback(data, parameter);
+		//判断map的Key是否存在
+		function containsKey(map, key) {
+			var result;
+			$.each(map, function(i, v) {
+				if (v.name == key) {
+					result = v.list;
+				}
+			});
+			return result;
 		}
 
 
@@ -340,10 +446,10 @@ define(function(require) {
 		//			jsonpCallback: 'highchartsJsonp4',
 		//			done: globalTools.ghnCallback
 		//		});
-function builtGhPage(data,id,parameter,dateFlag) {
+		function builtGhPage(data, id, parameter, dateFlag) {
 			var xData = new Array;
 			//var yestday = data.status.data.yestday;
-            var yestday = data;
+			var yestday = data;
 			dataItem = new Array;
 			$.each(yestday.totalData, function(i, v) {
 				dataItem = new Array;
@@ -365,9 +471,9 @@ function builtGhPage(data,id,parameter,dateFlag) {
 
 			//console.log(yestday);
 			//parameter.id = 'qrgnsp';
-            parameter.id = id;
+			parameter.id = id;
 			globalTools.ghnCallback(yestday, parameter);
-}
+		}
 
 	}());
 });
